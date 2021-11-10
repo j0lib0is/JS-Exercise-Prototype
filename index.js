d19c92d334c5f504584cd869241b878dd23a3e27
jacobjolibois <jacobjolibois@hey.com>
2021-11-10 13:07:48 -0600

    completed tasks 2 and 3

--- index.js
+++ index.js
@@ -81,9 +81,9 @@ const jacob = new Person('Jacob', 102);
         + ✅ should initialize with an `tank` at 0
         + ✅ should initialize with an `odometer` at 0
     - ✅ Give cars the ability to get fueled with a `.fill(gallons)` method. Add the gallons to `tank`.
-    - STRETCH: Give cars ability to `.drive(distance)`. The distance driven:
-        + Should cause the `odometer` to go up.
-        + Should cause the the `tank` to go down taking `milesPerGallon` into account.
+    - ✅ STRETCH: Give cars ability to `.drive(distance)`. The distance driven:
+        + ✅ Should cause the `odometer` to go up.
+        + ✅ Should cause the the `tank` to go down taking `milesPerGallon` into account.
     - STRETCH: A car which runs out of `fuel` while driving can't drive any more distance:
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
@@ -97,18 +97,27 @@ function Car(model, milesPerGallon) {
 Car.prototype.fill = function(gallons) {
   this.tank = this.tank + gallons;
 }
+Car.prototype.drive = function(distance) {
+  this.odometer = this.odometer + distance;
+  this.tank = this.tank - (distance / this.milesPerGallon);
+}
 
 
 /*
   TASK 3
-    - Write a Baby constructor subclassing Person.
-    - Besides `name` and `age`, Baby takes a third argument to initialize `favoriteToy`.
-    - Besides the methods on Person.prototype, babies have the ability to `.play()`:
-        + Should return a string "Playing with x", x being the favorite toy.
+    - ✅ Write a Baby constructor subclassing Person.
+    - ✅ Besides `name` and `age`, Baby takes a third argument to initialize `favoriteToy`.
+    - ✅ Besides the methods on Person.prototype, babies have the ability to `.play()`:
+        + ✅ Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
- 
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy;
 }
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function() {
+  return `Playing with ${this.favoriteToy}`
+};
 
 
 /* 
